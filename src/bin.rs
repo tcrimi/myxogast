82bd201ad05a20d7860242eaffe563f480a4f84f
kieran hervold <hervold@gmail.com>
2016-07-14 23:52:58 -0400

    working on a more useful executable, starting with argparse

--- src/bin.rs
+++ src/bin.rs
@@ -2,6 +2,7 @@ extern crate rustc_serialize;
 extern crate myxogast;
 extern crate bio;
 
+extern crate argparse;
 
 use std::io;
 use std::fs::File;
@@ -20,9 +21,35 @@ use myxogast::align::*;
 use myxogast::tree::*;
 use myxogast::seq::*;
 use myxogast::matrix::*;
-
+ 
+use argparse::{ArgumentParser, StoreTrue, Store};
 
 
 fn main() {
+    let mut ref_fname : String = String::new();
+    let mut query_fname : String = String::new();
+    { // scope block?
+        let mut parser = ArgumentParser::new();
+        parser.refer(&mut ref_fname)
+            .add_argument("ref", Store, "refernce JSON file")
+            .required();
+        parser.refer(&mut query_fname)
+            .add_argument("query", Store, "query file")
+            .required();
+        parser.parse_args_or_exit();
+    }
+    println!("fname: {}", ref_fname);
+    println!("fname: {}", query_fname);
 
+    //let graph = None;
+
+    let reader = fasta::Reader::from_file(query_fname).unwrap();
+    for gene in reader.records() {
+        let seq = gene.unwrap();
+ 
+        // FIXME: ugh ..
+        let seq_str : String = String::from_utf8_lossy( &seq.seq() ).into_owned();
+ 
+        let query = Sequence::from_str(seq_str.as_str()).unwrap();
+    }
 }
